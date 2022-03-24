4aeafd17e785c46fc5807f4468365b2c4bc2b67f
ewdlop <ray810815@gmail.com>
2022-03-23 21:33:13 -0400

    check in codes

---
+++ 881-890/881.BoatsToSavePeople.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeNote;
+
+public partial interface ISolution
+{
+    public int NumRescueBoats(int[] people, int limit)
+    {
+        Array.Sort(people);
+        int i = 0, j = people.Length - 1;
+        int ans = 0;
+
+        while (i <= j)
+        {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return ans;
+    }
+
+    public int NumberOfRescueBoats(ref int[] people, int limit, bool isSorted = false)
+    {
+        if(!isSorted)Array.Sort(people);
+        int i = 0, j = people.Length - 1;
+        int ans = 0;
+
+        while (i <= j)
+        {
+            ans++;
+            if (people[i] + people[j] <= limit)
+                i++;
+            j--;
+        }
+
+        return ans;
+    }
+    public int NumberOfRescueBoats(int[] people, int limit, bool isSorted = false)
+    {
+        int i = 0, j = people.Length - 1;
+        int ans = 0;
+        if (!isSorted)
+        {
+            ImmutableSortedSet<int> sortedPeople = people.ToImmutableSortedSet();
+            while (i <= j)
+            {
+                ans++;
+                if (sortedPeople[i] + sortedPeople[j] <= limit)
+                    i++;
+                j--;
+            }
+        }
+        else
+        {
+            while (i <= j)
+            {
+                ans++;
+                if (people[i] + people[j] <= limit)
+                    i++;
+                j--;
+            }
+        }
+        return ans;
+    }
+}
