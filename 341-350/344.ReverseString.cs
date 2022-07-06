5811e3b5de15bce20c8bb3fc735db393903913d9
ewdlop <ray810815@gmail.com>
2022-07-05 22:54:54 -0400

    a question

---
+++ 341-350/344.ReverseString.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public static void ReverseString(char[] s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
+        {
+            (s[left], s[right]) = (s[right--], s[left++]);
+        }
+    }
+}
