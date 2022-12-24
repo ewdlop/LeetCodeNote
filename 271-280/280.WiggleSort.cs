4b20c23fd504972d17860a42d229f358a2d8e46c
ewdlop <ray810815@gmail.com>
2022-12-24 10:19:57 -0500

    WiggleSort

---
+++ 271-280/280.WiggleSort.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public static void WiggleSort(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (i % 2 == 0 && nums[i] > nums[i + 1] ||
+               (i % 2 == 1 && nums[i] < nums[i + 1] ))
+            {
+                (nums[i], nums[i + 1]) = (nums[i + 1], nums[i]);
+            }
+        }
+    }
+}
