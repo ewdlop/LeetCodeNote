f318dcdac697718b4f93f6e00d317168aaf88709
ewdlop <ray810815@gmail.com>
2022-12-24 10:28:28 -0500

    MinMoves

---
+++ 451-460/453.MinimumMovesToEqualArrayElements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public static int MinMoves(int[] nums)
+    {
+        int sum = 0;
+        int min = int.MaxValue;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+        }
+        return sum - min * nums.Length;
+    }
+}
