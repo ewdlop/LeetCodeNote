094ff4e24b41144dc51f916712e3ec06c989a443
ewdlop <ray810815@gmail.com>
2023-12-24 13:12:22 -0500

    160.IntersectionOfTwoLinkedLists

---
+++ 151-160/160.IntersectionOfTwoLinkedLists.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        ListNode? pA = headA;
+        ListNode? pB = headB;
+        while (pA != pB)
+        {
+            pA = pA is null ? headB : pA.next;
+            pB = pB is null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
