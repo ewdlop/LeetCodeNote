758338adbb450cd0e73319eb5d7ca587143f731b
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2025-04-28 11:20:37 -0400

    Add MaxDepth method to calculate binary tree depth
    This commit introduces a new method `MaxDepth` in the `Solution` class under the `LeetCodeNote` namespace. The method computes the maximum depth of a binary tree using recursion, handling null input by returning 0. It evaluates the depths of the left and right subtrees and returns the greater value plus one.

---
+++ 101-110/104.MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null) return 0;
+
+        int L = MaxDepth(root.left);
+        int R = MaxDepth(root.right);
+        return Math.Max(L, R) + 1;
+    }
+}
