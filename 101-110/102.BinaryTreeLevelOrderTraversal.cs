ad9f68e7a24e02e40a5b1c6a2221e5491aea9507
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2025-04-28 11:02:14 -0400

    Visaul Studio: Add LevelOrder method for binary tree traversal
    Implement a new static partial class `Solution` with a `LevelOrder` method that performs level order traversal of a binary tree. The method returns a list of lists containing node values at each level, utilizing a queue for traversal and handling null nodes effectively.

---
+++ 101-110/102.BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeNote.Solution;
+
+public static partial class Solution
+{
+    public static IList<IList<int>> LevelOrder(TreeNode root)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            int size = queue.Count;
+            List<int> level = new List<int>();
+
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode node = queue.Dequeue();
+                level.Add(node.val);
+
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+
+            result.Add(level);
+        }
+
+        return result;
+    }
+}
