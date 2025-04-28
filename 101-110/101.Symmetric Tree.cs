08e3d376631be18f2400ff9440af8581c6c88602
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2025-04-28 09:57:56 -0400

    VisualStudio: Add symmetry check for binary trees in Solution class
    Introduces a new `Solution` class in the `LeetCodeNote` namespace with two static methods: `IsSymmetric` and `IsMirror`. The `IsSymmetric` method determines if a binary tree is symmetric by utilizing the `IsMirror` method, which recursively compares nodes for equality and checks their left and right subtrees. This implementation provides a structured approach to solving the symmetric tree problem.

---
+++ 101-110/101.Symmetric Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LeetCodeNote;
+
+public static partial class Solution
+{
+    public static bool IsSymmetric(TreeNode root)
+    {
+        return IsMirror(root.left, root.right);
+    }
+    public static bool IsMirror(TreeNode root1, TreeNode root2)
+    {
+
+        if (root1 == null && root2 == null)
+        {
+            return true;
+        }
+        if (root1 == null || root2 == null)
+        {
+            return false;
+        }
+        if (root1.val != root2.val)
+        {
+            return false;
+        }
+        bool left = IsMirror(root1.left, root2.right);
+        bool right = IsMirror(root1.right, root2.left);
+        return (left && right);
+
+    }
+}
+
+ ? true : false;
